5eee1d748487907248b2b61f4821b941ab6ab735
Adam Greloch <zplhatesbananas@gmail.com>
2023-03-22 22:29:45 +0100

    task1: optimize code, reduce byte size from 197 to 156

--- task1/inverse_permutation.asm
+++ task1/inverse_permutation.asm
@@ -9,49 +9,36 @@ inverse_permutation:
 ; Sprawdzanie poprawności wartości n
 ;*****************************************************************************
 .check_n:
-        cmp     rdi,0
-        je      .ret_false              ; jeśli n == 0, zwróć false
-        lea     r9,[rdi-1]
-        cmp     r9,0x7fffffff           ; porównaj n-1 z INT_MAX
-        jle     .inc_init               ; jeśli n <= INT_MAX + 1, jest ok
-                                        ; w.p.p. zwróć false
+        test    rdi,rdi
+        jz      .ret_false              ; jeśli n == 0, zwróć false
+        bt      rdi,31                  ; sprawdź, czy n <= 2^31 + 1
+        jnc     .check_range_init       ; jeśli tak, kontynuuj. w.p.p. zwróć false
 
 .ret_false:                             ; ustaw wynik funkcji na false i zakończ 
         xor     eax,eax
         ret
 
 ;*****************************************************************************
-; Inkrementacja wartości w tablicy o 1 oraz wstępne sprawdzenie tablicy p
+; Sprawdzenie zawartości tablicy p pod kątem zakresu liczb
 ;*****************************************************************************
-.inc_init:
-        mov     rcx,rdi                 ; zapisz n w rcx jako obecny licznik pętli
-        mov     eax,1                   ; eax -> wstępnie ustaw poprawność na true
-.inc:
+.check_range_init:
+        mov     ecx,edi                 ; zapisz n w ecx jako obecny licznik pętli
+.check_range_loop:
         cmp     [rsi+4*rcx-4],edi
-        jae     .undo_inc_bad           ; jeśli p[i] >= n, to p nie jest permutacją
+        jae     .ret_false              ; jeśli p[i] >= n, to p nie jest permutacją
         cmp     dword [rsi+4*rcx-4],0
-        jb      .undo_inc_bad           ; jeśli p[i] < 0, to p nie jest permutacją
-        inc     dword [rsi+4*rcx-4]     ; wykonaj p[i]++
-        loop    .inc
-
-        jmp .dups_loop_init             ; jeśli tablica p "przetrwała" pętlę .inc,
-                                        ; kontynuuj program
-
-.undo_inc_bad:
-        inc     rcx                     ; rcx -> cofnięcie inkr. dla i..n-1
-.undo_inc:
-        dec     dword [rsi+4*rcx]       ; wykonaj p[i]--
-        inc     rcx
-        cmp     rcx,rdi
-        jb      .undo_inc               ; jeśli i < n, kontynuuj pętlę
-        jmp     .ret_false
+        jb      .ret_false              ; jeśli p[i] < 0, to p nie jest permutacją
+        loop    .check_range_loop
 
 ;*****************************************************************************
-; Sprawdzenie poprawności tablicy p
+; Sprawdzenie poprawności tablicy p pod kątem duplikatów
 ;
 ; Poniższe instrukcje sprawdzają, czy p zawiera permutację liczb od 1 do n.
 ; Zawartość tablicy po wykonaniu instrukcji ma być taka, jak przed wykonaniem.
 ; Wynik sprawdzenia zawarty jest w buforze eax.
+;
+; Liczby są zaznaczone jako odwiedzone za pośrednictwem nieużywanego
+; najstarszego bitu.
 ; 
 ; Flaga w buforze r9: 
 ;       Jeśli r9 = 1, to poniższa pętla wprowadza zmiany do tablicy
@@ -59,81 +46,68 @@ inverse_permutation:
 ;*****************************************************************************
 .dups_loop_init:
         mov     r9,2                    ; r9 -> flaga dla pętli .check_dups
+        mov     ax,1                    ; eax -> wstępnie ustaw poprawność na true
 .dups_loop:
-        dec     r9                      ; r9 = 0
-        mov     rcx,rdi                 ; zapisz n w rcx jako obecny licznik pętli
+        dec     r9
+        mov     ecx,edi                 ; zapisz n w rcx jako obecny licznik pętli
 .check_dups:
         mov     edx,[rsi+4*rcx-4]       ; edx ma pełnić rolę indeksu j := p[i]
-        cmp     edx,0
-        jg      .skip_edx_neg           ; jeśli j > 0, przeskocz zmianę znaku
-        neg     edx                     ; w.p.p. j := -j
-.skip_edx_neg:
-        cmp     r9,0
+        btr     edx,31                  ; zignoruj 32-gi bit (TODO 31/32?)
+        test    r9,r9
         je      .dups_undo              ; jeśli r9 = 0, wycofuj zmiany
 .dups_do:
-        cmp     dword [rsi+4*rdx-4],0
-        jg      .dups_neg               ; jeśli p[j] > 0, kontynuuj pętlę
+        bt      dword [rsi+4*rdx],31
+        jnc     .dups_neg               ; jeśli nieodwiedzone, kontynuuj pętlę
         xor     eax,eax                 ; w.p.p. znaleziono duplikat
         jmp     .skip_p_neg
 .dups_undo:
-        cmp     dword [rsi+4*rdx-4],0
-        jg      .skip_p_neg             ; jeśli p[j] > 0, to nie ruszaj
+        bt      dword [rsi+4*rdx],31
+        jnc     .skip_p_neg             ; jeśli ost bit = 0, to nie ruszaj
 .dups_neg:
-        neg     dword [rsi+4*rdx-4]
+        btc     dword [rsi+4*rdx],31    ; XORuj ost bit
 .skip_p_neg:
         loop    .check_dups
 
-        cmp     r9,0
-        jg      .dups_loop               ; jeśli r9 > 0, to kontynuuj pętlę
+        test    r9,r9
+        jnz     .dups_loop              ; jeśli r9 > 0, to kontynuuj pętlę
 
-        cmp     eax,0
-        je      .undo_inc_full
+        test    eax,eax
+        jz      .ret_false
         ; Jeśli eax = 0, to znaleziono duplikaty, więc wejściowa tablica jest
-        ; niepoprawna, należy wycofać wszelkie zmiany za pomocą instrukcji pod
-        ; etykietą .undo_inc_full i zwrócić z eax = 0.
+        ; niepoprawna.
 
 ;*****************************************************************************
 ; Odwrócenie permutacji (algorytm Huanga, Knuth I, str. 182)
 ;
-; Zakłada, że tablica p wskazuje poprawną permutację n liczb od 1 do n, oraz,
+; Zakłada, że tablica p wskazuje poprawną permutację n liczb od 0 do n-1, oraz,
 ; że n jest liczbą z zakresu 1..2^31+1.
 ;*****************************************************************************
 .huang_init:
-        mov     r8,rbx                  ; przechowaj poprzednią wartość rbx
-        mov     rcx,rdi                 ; rcx -> m := n (licznik pętli)
-        ; Jest gwarantowane, że rdi jest liczbą 32 bitową, więc powyższa
-        ; operacja ustawia jedynie najmłodsze 32 bity.
-        ; Wszystkie kolejne odwołania do zawartości rcx będą więc za
-        ; pośrednictwem ecx.
-        mov     ebx,-1                  ; ebx -> j := -1
+        mov     ecx,edi                 ; ecx -> m := n (licznik pętli)
+        mov     eax,-1                  ; eax -> j := -1
+.huang_inc: 
+        inc     dword [rsi+4*rcx-4]
+        loop    .huang_inc
+        mov     ecx,edi
 .huang_next:
         mov     edx,[rsi+4*rcx-4]       ; edx -> i := p[m-1]
-        cmp     edx,0
+        test    edx,edx
         jl      .huang_cycle_end        ; jeśli i < 0, przeskocz do końca cyklu
 .huang_invert:
-        mov     [rsi+4*rcx-4],ebx       ; p[m-1] := j
-        mov     ebx,ecx                 ; (1/2) j := -m
-        neg     ebx                     ; (2/2)
+        mov     [rsi+4*rcx-4],eax       ; p[m-1] := j
+        mov     eax,ecx                 ; (1/2) j := -m
+        neg     eax                     ; (2/2)
         mov     ecx,edx                 ; m := i
         mov     edx,[rsi+4*rcx-4]       ; i := p[m-1]
 .huang_is_end:
-        cmp     edx,0
+        test    edx,edx
         jg      .huang_invert           ; jeśli i > 0, wróć do invert
-        mov     edx,ebx                 ; w.p.p. i := j
+        mov     edx,eax                 ; w.p.p. i := j
 .huang_cycle_end:
         mov     [rsi+4*rcx-4],edx       ; (1/2) p[m-1] := -i
         neg     dword [rsi+4*rcx-4]     ; (2/2)
+        dec     dword [rsi+4*rcx-4]
         loop    .huang_next
 
-        mov     rbx,r8                  ; przywróć poprzednią wartość rbx
-
-;*****************************************************************************
-; Dekrementacja wartości w tablicy o 1
-;*****************************************************************************
-.undo_inc_full:
-        mov     rcx,rdi                 ; ustaw licznik pętli i na n
-.undo_inc_loop:
-        dec     dword [rsi+4*rcx-4]     ; wykonaj p[i]--
-        loop    .undo_inc_loop
+        mov     ax,1
         ret
-
